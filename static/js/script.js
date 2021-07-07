b6acc4748df9daee781425910e01ab9827ab8da0
4bhis1 <absk8634@gmail.com>
2021-07-07 23:39:45 +0530

    done with project section

--- static/js/script.js
+++ static/js/script.js
@@ -38,3 +38,16 @@ setInterval(function () {
     }
 },300)
 
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
